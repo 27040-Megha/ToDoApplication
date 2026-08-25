2a3e7b3d9870e5fe02c3238c362b8a488edf24cf
27040-Megha <megha.easwaramoorthy@solitontech.com>
2026-08-25 16:30:17 +0530

    feat: Add recurring tasks and View all Daily Tasks

---
+++ ToDoApplication/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApplication.Model
+{
+    public class User
+    {
+    }
+}
---
+++ ToDoApplication/Repository/TaskRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApplication.Model;
+
+namespace ToDoApplication.Repository
+{
+    public class TaskRepo
+    {
+        public void AddToDoTasks(List<Tasks> toDoTasks)
+        {
+            foreach (var tasks in toDoTasks)
+            {
+                var listOfTasks = FileRepoService.ReadFile<Tasks>(FilePath.TaskFile);
+                listOfTasks.Add(tasks);
+                FileRepoService.WriteFile(listOfTasks, FilePath.TaskFile);
+            }
+        }
+        
+        public List<Tasks> ReturnAllToDoTasks()
+        {
+            var listOfTasks = FileRepoService.ReadFile<Tasks>(FilePath.TaskFile);
+            return listOfTasks;
+        }
+    }
+}
